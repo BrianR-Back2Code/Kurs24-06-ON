d85731821f6db8cfdcb22ec3858fb698d0e3faaf
Brian <brre0991@gmail.com>
2024-10-11 14:27:00 +0200

    Quiz fertig gestellt

--- JavaScript/Hausaufgabe/11.10.2024/Bonusaufgabe/quiz.js
+++ JavaScript/Hausaufgabe/11.10.2024/Bonusaufgabe/quiz.js
@@ -3,12 +3,36 @@ const Frage_2 = "Hast du deine Hausaufgaben gemacht?";
 const Frage_3 = "Wieviele Finger werden gezeigt?";
 
 let score = 0;
-
-let a = prompt("Frage 1 : " + Frage1);
-if (a == "Nein") {
-  alert("Antwort " + a + " ist Richtig");
-  score = score + 1;
+if (score == -2) {
 } else {
-  alert;
+  let a = prompt("Frage 1 : " + Frage_1);
+  if (a == "Nein") {
+    alert("Antwort " + a + " ist Richtig");
+    score = score + 1;
+  } else {
+    alert("Das ist Falsch");
+    score = score - 1;
+  }
+
+  let b = prompt("Frage 2 : " + Frage_2);
+  if (b == "Ja") {
+    alert("Antwort " + b + " ist Richtig");
+    score = score + 1;
+  } else {
+    alert("Das ist Falsch");
+    score = score - 1;
+  }
+
+  let c = prompt("Frage 3 : " + Frage_3);
+  if (c == "Keine") {
+    alert("Antwort " + c + " ist Richtig");
+    score = score + 1;
+  } else {
+    alert("Das ist Falsch");
+    score = score - 1;
+  }
 }
+
 console.log(a);
+console.log(b);
+console.log(c);
