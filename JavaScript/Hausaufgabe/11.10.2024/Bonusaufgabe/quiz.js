5b075c8b2bf7f689db1c2bda4618c1070d001296
Brian <brre0991@gmail.com>
2024-10-11 13:15:56 +0200

    Hallo

--- JavaScript/Hausaufgabe/11.10.2024/Bonusaufgabe/quiz.js
+++ JavaScript/Hausaufgabe/11.10.2024/Bonusaufgabe/quiz.js
@@ -1,6 +1,14 @@
-let Frage1 = "Hallo";
+const Frage_1 = "Wird 'var' noch oft benutzt?";
+const Frage_2 = "Hast du deine Hausaufgaben gemacht?";
+const Frage_3 = "Wieviele Finger werden gezeigt?";
+
+let score = 0;
+
 let a = prompt("Frage 1 : " + Frage1);
-if (a == "Hallo") {
+if (a == "Nein") {
   alert("Antwort " + a + " ist Richtig");
+  score = score + 1;
+} else {
+  alert;
 }
 console.log(a);
