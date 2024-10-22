d2de67109fff4dd933db10baa846577abc1ef9fb
Brian <brre0991@gmail.com>
2024-10-22 14:50:23 +0200

    Hausaufgaben Done

--- React/Hausaufgaben/hausaufgaben_react/src/App.js
+++ React/Hausaufgaben/hausaufgaben_react/src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
-import "./component/functions.js";
+import Welcome from "./component/Welcome";
 
 function App() {
-  let myname = "Brian";
   return (
     <>
-      <div>
-        <h1>Willkommen bei meiner React App</h1>
-        <h2>Hallo {myname}</h2>
-        <h3 style={{ color: "red" }}>Rechnung 2 + 2 = {2 + 2}</h3>
-        <button id="clickme">Click me</button>
-        <button onClick={() => alert("You clicked the button")}>
-          Show alert
-        </button>
-      </div>
+      <Welcome />
     </>
   );
 }
