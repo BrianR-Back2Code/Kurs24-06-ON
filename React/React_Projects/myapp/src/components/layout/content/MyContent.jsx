03b96e8dba7536d9d79817a4790499dda0dfd26a
Brian <brre0991@gmail.com>
2024-10-25 09:23:37 +0200

    Immer noch keine Ahnung

--- React/React_Projects/myapp/src/components/layout/content/MyContent.jsx
+++ React/React_Projects/myapp/src/components/layout/content/MyContent.jsx
@@ -3,7 +3,7 @@ import { Button, Container, Typography } from "@mui/material";
 function MyContent() {
   return (
     <Container sx={{ bgcolor: "grey", height: "100vh" }}>
-      <Button variant="contained" disabled onClick={() => alert("Crackhead")}>
+      <Button variant="contained" onClick={() => alert("Crackhead")}>
         Hallo
       </Button>
     </Container>
