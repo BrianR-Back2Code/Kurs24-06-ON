8254de65695a3a93d6bc6b40f322afee8f7a2c97
ICantCodeCrack <brre0991@gmail.com>
2024-12-04 17:19:29 +0100

    Hausaufgaben done

--- jest/math.js
+++ jest/math.js
@@ -7,7 +7,22 @@ class Calculator {
   }
 
   divide(a, b) {
+    if (b === 0) {
+      throw new Error("Division durch 0 nicht möglich ");
+    }
     return a / b;
+  }
+
+  even(number) {
+    return number % 2 === 0;
+  }
+
+  filterpositivenumbers(numbers) {
+    return numbers.filter((num) => num > 0);
+  }
+
+  async fetch() {
+    return "Echte Daten";
   }
 }
 
